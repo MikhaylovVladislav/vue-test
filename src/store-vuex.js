53ee82c57d28a7283d41ea097f2bc0f307c95fc0
MikhaylovVladislav <79971836+MikhaylovVladislav@users.noreply.github.com>
2023-03-23 01:34:47 +0500

    Разработал начальное хранилище

---
+++ src/store-vuex.js
@@ -0,0 +1,27 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state:{
+    count:0,
+    name: 'Vladds'
+  },
+  mutations:{
+    incriment(state){
+      state.count++;
+    }
+  },
+  actions:{
+    increment(context){
+      context.commit('incriment');
+    }
+  },
+  getters:{
+    count: state => state.count,
+    name: state => state.name
+  }
+})
+
+export default store
