eb88417b99c5a1d635e030fbcc769d3c858b4165
MikhaylovVladislav <79971836+MikhaylovVladislav@users.noreply.github.com>
2023-03-24 17:36:28 +0500

    => добавил обработку пути для usersPage

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import newsComponent from './components/ContentArea/news-component/news-componen
 import messengerComponent from './components/ContentArea/messenger-component/messenger-component.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import store from '@/store'
+import usersComponent from '@/components/ContentArea/users-component/users-component.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/profile', component: profileComponent },
     { path: '/news', component: newsComponent },
-    { path: '/messenger', component: messengerComponent }
+    { path: '/messenger', component: messengerComponent },
+    { path: '/users', component: usersComponent }
   ]
 })
 
